e4f9c8909f5fa1c4cf466699e2beaef021301c60
kungru <108171226+kungru@users.noreply.github.com>
2022-07-25 21:45:25 +0700

    sual3

--- mainpage/index.js
+++ mainpage/index.js
@@ -1,19 +1,31 @@
 //Get the button:
-mybutton = document.getElementById("myBtn");
+
+{mybutton = document.getElementById("myBtn");
 
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
-
-
 function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
+}
+}
+
+{
+bar = document.getElementsById("bar");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    bar.style.display = "flex";
+  } else {
+    bar.style.display = "none";
+  }
+}
 }
