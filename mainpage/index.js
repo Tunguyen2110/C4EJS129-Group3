0eb3af6ad16a346bcc18190247a8ddc468598924
kungru <108171226+kungru@users.noreply.github.com>
2022-08-03 19:05:18 +0700

    123

--- mainpage/index.js
+++ mainpage/index.js
@@ -6,8 +6,15 @@ cart = document.querySelector(".cart");
 
 window.onscroll = function () {scrollFunction()};
 
+<<<<<<< HEAD
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+=======
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+>>>>>>> parent of 7e5eb88 (234)
     mybutton.style.display = "block";
     bar.style.display = "flex";
     cart.style.display = "flex";
@@ -23,6 +30,7 @@ function topFunction() {
 }
 }
 
+<<<<<<< HEAD
 // {
 bar = document.querySelector(".bar");
 cart = document.querySelector(".cart");
@@ -45,4 +53,16 @@ function showupcheck() {
 }
 function notshowupcheck(){
   cardcheck.style.display = "none";
+=======
+{
+bar = document.getElementsById("bar");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    bar.style.display = "flex";
+  } else {
+    bar.style.display = "none";
+  }
+}
+>>>>>>> parent of 7e5eb88 (234)
 }
