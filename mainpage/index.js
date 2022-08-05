9d05a0a5bdc2577c960fbd4fe41732e8103d641e
kungru <108171226+kungru@users.noreply.github.com>
2022-08-05 13:03:29 +0700

    123

--- mainpage/index.js
+++ mainpage/index.js
@@ -1,27 +1,15 @@
 //Get the button:
 
 {mybutton = document.getElementById("myBtn");
-bar = document.querySelector(".bar");
-cart = document.querySelector(".cart");
+
 
 window.onscroll = function () {scrollFunction()};
 
-<<<<<<< HEAD
 function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-=======
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
->>>>>>> parent of 7e5eb88 (234)
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     mybutton.style.display = "block";
-    bar.style.display = "flex";
-    cart.style.display = "flex";
   } else {
     mybutton.style.display = "none";
-    bar.style.display = "none";
-        cart.style.display = "none";
   }
 }
 function topFunction() {
@@ -30,21 +18,20 @@ function topFunction() {
 }
 }
 
-<<<<<<< HEAD
-// {
+{
 bar = document.querySelector(".bar");
 cart = document.querySelector(".cart");
-// window.onscroll = function() {scrollFunction()};
-// function scrollFunction() {
-//   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-//     bar.style.display = "flex";
-//     cart.style.display = "flex";
-//   } else {
-//     bar.style.display = "none";
-//     cart.style.display = "none";
-//   }
-// } 
-// }
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    bar.style.display = "flex";
+    cart.style.display = "flex";
+  } else {
+    bar.style.display = "none";
+    cart.style.display = "none";
+  }
+} 
+}
 
 
 const cardcheck = document.querySelector(".card-check");
@@ -53,16 +40,4 @@ function showupcheck() {
 }
 function notshowupcheck(){
   cardcheck.style.display = "none";
-=======
-{
-bar = document.getElementsById("bar");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    bar.style.display = "flex";
-  } else {
-    bar.style.display = "none";
-  }
-}
->>>>>>> parent of 7e5eb88 (234)
 }
