a0467297ef73e660f9c8da4b3e29d7ce39bf8ee4
kungru <108171226+kungru@users.noreply.github.com>
2022-08-05 13:23:25 +0700

    sual2

--- mainpage/index.js
+++ mainpage/index.js
@@ -1,24 +1,25 @@
-//Get the button:
+// //Get the button:
 
-{mybutton = document.getElementById("myBtn");
+// {mybutton = document.getElementById("myBtn");
 
 
-window.onscroll = function () {scrollFunction()};
+// window.onscroll = function () {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-}
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0; 
+//   document.documentElement.scrollTop = 0; 
+// }
+// }
 
 {
+mybutton = document.getElementById("myBtn");
 bar = document.querySelector(".bar");
 cart = document.querySelector(".cart");
 window.onscroll = function() {scrollFunction()};
@@ -26,14 +27,14 @@ function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     bar.style.display = "flex";
     cart.style.display = "flex";
+    mybutton.style.display = "block";
   } else {
     bar.style.display = "none";
     cart.style.display = "none";
+    mybutton.style.display = "none";
   }
 } 
 }
-
-
 let listBtnPlus = document.querySelectorAll('div.card-plus');
 let listBtnMinus = document.querySelectorAll('div.card-minus');
 let listCardCheck = document.querySelectorAll('div.card-check');
@@ -44,3 +45,4 @@ for (let i = 0; i < listBtnPlus.length; i++) {
   listBtnMinus[i].addEventListener('click', (e) => {
     listCardCheck[i].style.display = "none";
   })};
+
