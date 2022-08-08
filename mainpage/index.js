968f7bd999b67894f2b799ed05ee564b54747aa8
kungru <108171226+kungru@users.noreply.github.com>
2022-08-08 16:42:15 +0700

    xemthem

--- mainpage/index.js
+++ mainpage/index.js
@@ -2,7 +2,7 @@
   mybutton = document.getElementById("myBtn");
   bar = document.querySelector(".bar");
   cart = document.querySelector(".cart");
-  window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () { scrollFunction() };
   function scrollFunction() {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
       bar.style.display = "flex";
@@ -13,20 +13,42 @@
       cart.style.display = "none";
       mybutton.style.display = "none";
     }
-  } 
   }
-  let listBtnPlus = document.querySelectorAll('div.card-plus');
-  let listBtnMinus = document.querySelectorAll('div.card-minus');
-  let listCardCheck = document.querySelectorAll('div.card-check');
-  for (let i = 0; i < listBtnPlus.length; i++) {
-    listBtnPlus[i].addEventListener('click', (e) => {
-      listCardCheck[i].style.display = "flex";
-    })
-    listBtnMinus[i].addEventListener('click', (e) => {
-      listCardCheck[i].style.display = "none";
-    })};
+}
+function topFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
-    function change_background()
-    {
-       document.querySelector("body").style.background = 'red';
-    }
+
+let listBtnPlus = document.querySelectorAll('div.card-plus');
+let listBtnMinus = document.querySelectorAll('div.card-minus');
+let listCardCheck = document.querySelectorAll('div.card-check');
+for (let i = 0; i < listBtnPlus.length; i++) {
+  listBtnPlus[i].addEventListener('click', (e) => {
+    listCardCheck[i].style.display = "flex";
+  })
+  listBtnMinus[i].addEventListener('click', (e) => {
+    listCardCheck[i].style.display = "none";
+  })
+};
+
+
+let sidebar_hid = document.querySelector(".sidebar-plc-hidden");
+let xemthem = document.querySelector(".xemthem");
+let count = 1;
+console.log(sidebar_hid);
+
+
+function xemthem_sidebar() {
+  if (count === 1) {
+    sidebar_hid.style.display = 'block';
+    xemthem.innerHTML = "Kéo lên";
+    count++;
+  } else {
+    sidebar_hid.style.display = 'none';
+    xemthem.innerHTML = "Xem thêm";
+    count = 1;
+  }
+
+} 
