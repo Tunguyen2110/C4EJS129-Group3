1698073ef75975cf0a784ab0941c1e15542f5725
kungru <108171226+kungru@users.noreply.github.com>
2022-08-08 21:04:08 +0700

    123

--- mainpage/index.js
+++ mainpage/index.js
@@ -52,3 +52,11 @@ function xemthem_sidebar() {
   }
 
 } 
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
