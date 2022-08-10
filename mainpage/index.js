14b1fad6c27a2a3a02bb884becb5d2e5c298a20c
kungru <108171226+kungru@users.noreply.github.com>
2022-08-10 20:00:33 +0700

    123

--- mainpage/index.js
+++ mainpage/index.js
@@ -150,24 +150,145 @@ console.log(att);
 
 
 //filter
-const nodes=document.querySelectorAll('.card-type');
-console.log(nodes);
-const texts = [...nodes].map(n => n.textContent);
-console.log(texts);
-const filtered = [...nodes].filter(n => n.textContent.includes('Phường 4'));
-console.log(filtered);
-// const li=document.querySelector('.container-item-location-li');
-// console.log(li)
-// document.querySelector('.container-item-location-li').onclick(function(){
-//   document.querySelectorAll('.card').style.display = "none";
-//   document.write('${filtered}')
-
-// })
-
-function ttat(){
- let card_hide= document.querySelectorAll('.card');
- card_hide.forEach(card => {
-card.style.display ="none";
- });
- document.querySelectorAll('.card').innerHTML = parent.forEach.filtered;
+const data= [
+  {
+    image: "../media/spck/ham2.jpg",
+    type: "Danh lam thắng cảnh - Phường 4",
+    price: "Price",
+    title: "Đường hầm điêu khắc Đà Lạt",
+    location: " Phường 4 - Tp. Đà Lạt - Lâm Đồng",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/_MG_7157.jpg",
+    type: "Khu vui chơi, công viên - Phường 4",
+    price: "Price",
+    title: "Khu vui chơi Đà Lạt Wonderland",
+    location: " 19 Đ. Hoa Hồng - Phường 4 - Tp. Đà Lạt - Lâm Đồng",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/tuyenlam.jpg",
+    type: "Danh lam thắng cảnh - Phường 4",
+    price: "Price",
+    title: "Happy Hill Đà Lạt",
+    location: " Khu du lịch sinh thái - nhà hàng Đá Tiên - hồ Tuyền Lâm",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/rungthong4.jpg",
+    type: "Nhà hàng - Phường 4",
+    price: "Price",
+    title: "Rừng Thông Mơ Farm & Bistro",
+    location: "Đ. Hoa Hồng - Phường 4 - Tp. Đà Lạt - Lâm Đồng ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/land4.png",
+    type: "Khu vui chơi, công viên - Phường 5",
+    price: "Price",
+    title: "Vùng Đất Cổ Tích Fairytale Land Đà Lạt",
+    location: " Đ. Hoa Hồng - Phường 4 - Tp. Đà Lạt - Lâm Đồng ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/qij6cdhlwescboqpkhaz.webp",
+    type: "Khu vui chơi, công viên - Phường 5",
+    price: "Price",
+    title: "Làng Hoa Vạn Thành",
+    location: " 40 Vạn Thành - Phường 5 - Tp. Đà Lạt - Lâm Đồng ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/hoa-son-dien-trang-da-lat-6.jpg",
+    type: "Danh lam thắng cảnh - Phường 5",
+    price: "Price",
+    title: "Khu Du Lịch Sinh Thái Hoa Sơn Điền Trang",
+    location: " 1D Hoàng Văn Thụ - Tiểu khu 159 Phường 5 hướng đi đèo Tà Nung ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/xiumai.jpg",
+    type: "Ẩm thực địa phương - Phường 5",
+    price: "Price",
+    title: "Bánh Mì Xíu Mại Hoàng Diệu",
+    location: " 26 đường Hoàng Diệu - phường 5 - Tp. Đà Lạt ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/lac_tien_goi_3.jpg",
+    type: "Danh lam thắng cảnh - Phường 5",
+    price: "Price",
+    title: "Lạc Tiên Giới Đà Lạt",
+    location: " 1/3 Lâm Sinh - Phường 5 - Tp Đà Lạt - Lâm Đồng ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/nhatho.jpg",
+    type: "Công trình văn hóa kiến trúc - Phường 6",
+    price: "Price",
+    title: "Nhà Thờ Domaine De Marie Đà Lạt",
+    location: " 1 Ngô Quyền - phường 6 - Tp. Đà Lạt - tỉnh Lâm Đồng ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/ham2.jpg",
+    type: "Nhà hàng - Phường 6",
+    price: "50,000đ - 200,000đ",
+    title: "Ốc Nhồi Thịt 33",
+    location: " 33 đường Hai Bà Trưng - Phường 6 - Tp. Đà Lạt ",
+    more: "Xem thêm",
+  },
+  {
+    image: "../media/spck/trieudoahong.jpg",
+    type: "Quán cà phê - Phường 6",
+    price: "Price",
+    title: "Cà Phê Triệu Đóa Hồng",
+    location: " 25 Đường Thi Sách - Phường 6 - Tp. Đà Lạt ",
+    more: "Xem thêm",
+  },
+]
+let stringHTML = "";
+const container_item_main = document.getElementById("container-item-main");
+for(let element of data){
+  stringHTML += `
+                <div class="card">
+                    <img src=${element.image} class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="card-type" > ${element.type}</div>
+                        <div class="card-price"><i class="fa-solid fa-tag"></i> ${element.price}</div>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-location"><i class="fa-solid fa-location-dot"></i> ${element.location}</p>
+                        <a href="#" class="btn btn-primary" id="card-footer">${element.more}</a>
+                        <div class="card-plus"><i class='bx bx-plus-circle'></i></div>
+                        <div class="card-minus"><i class='bx bx-minus-circle'></i></div>
+                        <div class="card-check"><i class='bx bx-checkbox-checked'></i></div>
+                    </div>
+                </div>
+  `
 }
+container_item_main.innerHTML = stringHTML;
+
+console.log(stringHTML);
+
+
+
+const filtered=data.filter(function(str){
+  return str.includes("Phường 4");
+});
+console.log(str);
+
+// const nodes=document.querySelectorAll('.card-type');
+// console.log(nodes);
+// const texts = [...nodes].map(n => n.textContent);
+// console.log(texts);
+// const filtered = [...nodes].filter(n => n.textContent.includes('Phường 4'));
+// console.log(filtered);
+
+// function ttat(){
+//  let card_hide= document.querySelectorAll('.card');
+//  card_hide.forEach(card => {
+// card.style.display ="none";
+//  });
+//  document.querySelectorAll('.card').innerHTML = parent.forEach.filtered;
+// }
